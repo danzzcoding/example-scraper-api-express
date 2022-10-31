f6c79f6d7d30538565479035e2d5727bed6e2158
Danzz Coding <danzzcoding@gmail.com>
2022-10-31 18:55:00 +0700

    Update main.js

--- route/main.js
+++ route/main.js
@@ -3,7 +3,6 @@ __path = process.cwd()
 // Module
 const express = require('express');
 const route = express.Router();
-const fetch = require('node-fetch');
 
 route.get('/', (req, res) => {
     res.sendFile(__path + '/pages/index.html')
